fd80ee46b869a292da1f7df2c7963a3e6a2edc7e
BINKSb1aCKt0rCH <esteban.marielouise@gmail.com>
2023-05-31 15:21:31 +0200

    Refractor project

---
+++ src/main/java/fr/montreuil/iut/towerdefense/controller/LoreController.java
@@ -0,0 +1,35 @@
+package fr.montreuil.iut.towerdefense.controller;
+
+import fr.montreuil.iut.towerdefense.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoreController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @Override
+    public void initialize(URL url, ResourceBundle ressourceBundle) {
+    }
+
+    @FXML
+    void defendre(ActionEvent event) throws IOException {
+        System.out.println("Click sur defendre Arrassia");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Vue.fxml"));
+        root = fxmlLoader.load();
+        //Controller controller = fxmlLoader.getController();
+        stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
