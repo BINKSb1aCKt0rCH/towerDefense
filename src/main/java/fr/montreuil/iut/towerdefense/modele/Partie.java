37799b13cfe2d3171d6536363d7981bded845461
BINKSb1aCKt0rCH <esteban.marielouise@gmail.com>
2023-06-02 15:37:01 +0200

    modif git ignore

--- src/main/java/fr/montreuil/iut/towerdefense/modele/Partie.java
+++ src/main/java/fr/montreuil/iut/towerdefense/modele/Partie.java
@@ -1,22 +1,27 @@
 package fr.montreuil.iut.towerdefense.modele;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 
 public class Partie {
     MapModele mapModele;
     private int width,height;
-    private ArrayList<Monstre>monstres;
-    public Partie(int width,int height){
+    private int berrys ;
+    private ObservableList<Monstre> monstres;public Partie(int width,int height){
         this.width = width;
         this.height=height;
-        this.monstres = new ArrayList<>();
+        this.monstres = FXCollections.observableArrayList();
         this.mapModele = new MapModele();
 
     }
     public void ajouter(Monstre m){
         monstres.add(m);
     }
-
+    public ObservableList<Monstre> getMonstres(){
+        return monstres;
+    }
     public boolean dansTerrain(int x, int y){return x < 5 && y <9 && x >0 && y>0;}
 
     public void unTour(){
